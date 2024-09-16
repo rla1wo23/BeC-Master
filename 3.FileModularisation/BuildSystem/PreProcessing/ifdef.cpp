970e0c3c43e6478efbd13d404cf6f9e47cb66184
rla1wo23@gmail.com <rla1wo23@gmail.com>
2024-09-16 11:11:43 +0900

    add: ifdef preprocessor

---
+++ 3.FileModularisation/BuildSystem/PreProcessing/ifdef.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#define T1 // If you want to know how this code works, comment out this part.
+class test1 {
+public:
+  virtual void print() { std::cout << "T1 is not Defined\n"; }
+};
+
+#ifdef T1 // if T1 defined, then below this codes could be runned
+class test2 : public test1 {
+public:
+  void print() override { std::cout << "T1 is Defined\n"; }
+};
+#else
+// If T1 is undefined, test2 behaves the same as test1.
+typedef test1 test2;
+#endif
+
+int main() {
+  test2 obj;
+  obj.print();
+  return 0;
+}
