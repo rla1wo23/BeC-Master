9325b0118d0ef2d4baa74573698b879f7638719e
rla1wo23@gmail.com <rla1wo23@gmail.com>
2024-09-16 13:50:26 +0900

    add: CMake Example

---
+++ 3.FileModularisation/CMake/build/CMakeFiles/addExampleExe.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/addExampleExe.dir/main.cpp.o"
+  "CMakeFiles/addExampleExe.dir/main.cpp.o.d"
+  "CMakeFiles/addExampleExe.dir/math_operations.cpp.o"
+  "CMakeFiles/addExampleExe.dir/math_operations.cpp.o.d"
+  "addExampleExe"
+  "addExampleExe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/addExampleExe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 3.FileModularisation/CMake/build/CMakeFiles/addExampleExe.dir/math_operations.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/addExampleExe.dir/math_operations.cpp.o: \
+  /Users/jazzmaster/Developer/cppMaster/3.FileModularisation/CMake/math_operations.cpp \
+  /Users/jazzmaster/Developer/cppMaster/3.FileModularisation/CMake/math_operations.h
